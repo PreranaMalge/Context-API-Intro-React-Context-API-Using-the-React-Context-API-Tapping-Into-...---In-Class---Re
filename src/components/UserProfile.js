ea25fd36c328fa9902eb68db3e600d14cb65166a
PreranaMalge <preranamalge123@gmail.com>
2022-05-31 23:55:18 +0530

    assignment

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,15 +1,15 @@
-import { useContext } from 'react'
-import React from 'react'
-import { UserContext } from "./App"
+import { useContext } from "react";
+import React from "react";
+import { UserContext } from "./App";
 
-const UserProfile = () =>{
-    const localvar = useContext(UserContext);
-    return(
-        <>
-        <h1 id='name'>Name:-{localvar.name}</h1>
-        <h1 id='age'>Age:-{localvar.age}</h1>
-        </>
-    )
-}
+const UserProfile = () => {
+  const localVar = useContext(UserContext);
+  return (
+    <>
+      <h1 id="name">Name:- {localVar.name}</h1>
+      <h1 id="age">Age:- {localVar.age}</h1>
+    </>
+  );
+};
 
-export {UserProfile}
+export { UserProfile };
